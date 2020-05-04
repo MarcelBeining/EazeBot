62c29f2466a225fc4816fe217270f629aec614cd
Marcel <marcel.beining@gmail.com>
2020-05-04 09:58:54 +0200

    allow definition of user dir via arguments allow abbreviations for arguments

--- eazebot/main.py
+++ eazebot/main.py
@@ -1,16 +1,32 @@
 import argparse
+import os
+
+def check_dir_arg(folder):
+    if not os.path.isdir(folder):
+        raise NotADirectoryError(f"Argument '{folder}' is no known directory.")
+    return os.path.abspath(folder)
 
 
 # execute main if running as script
 def main(sysargv = None):
+    # execute main if running as script
     parser = argparse.ArgumentParser(description='Processes input to eazebot main function.')
-    parser.add_argument('--init', dest='init', action='store_true', required=False,
+    parser.add_argument('-i', '--init', dest='init', action='store_true', required=False,
                         help='copies the necessary user config files to the current directory')
-    parser.add_argument('--config', dest='config', action='store_true', required=False,
+    parser.add_argument('-c', '--config', dest='config', action='store_true', required=False,
                         help='calls a dialog to fill out the configs interactively')
-    parser.add_argument('--no-warning', dest='warning', action='store_false', required=False,
+    parser.add_argument('-n', '--no-warning', dest='warning', action='store_false', required=False,
                         help='does not warn for preexisting config files when running the --init flag')
+    parser.add_argument('-d', '--user-dir', dest='user_dir', default=None, type=check_dir_arg, required=False,
+                        help="Absolute or relative path to the user folder")
+
+    parser.parse_args('-ic'.split())
+
     args = parser.parse_args(sysargv)
+
+    # change current path to user directory if argument exists
+    if args.user_dir is not None:
+        os.chdir(args.user_dir)
 
     if args.init:
         from eazebot.auxiliaries import copy_init_files
