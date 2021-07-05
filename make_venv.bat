70defcfae93a196ea6630d4ede53bbbe2e6cf42a
Marcel <marcel.beining@gmail.com>
2021-07-05 12:12:42 +0200

    fix

---
+++ make_venv.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+setlocal enabledelayedexpansion
+cd %~dp0
+python -c "import virtualenv"
+IF errorlevel 1 (
+echo Installing virtualenv
+call python -m pip install virtualenv -U --no-cache --ignore-installed
+)
+IF EXIST %~dp0venv (
+echo venv already exists
+) ELSE (
+echo Creating venv
+call python -m virtualenv %~dp0venv
+)
+
+call venv\Scripts\activate.bat && python -m pip install -r requirements.txt 
+
+pause
